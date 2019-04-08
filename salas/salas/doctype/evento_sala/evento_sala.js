e117c3692d4c6e9056e37b96e0aeb087b71bd03e
chabito79 <rubenguzmancastro@gmail.com>
2019-04-08 16:36:03 +0000

    Comparacion de horas

--- salas/salas/doctype/evento_sala/evento_sala.js
+++ salas/salas/doctype/evento_sala/evento_sala.js
@@ -6,7 +6,7 @@ frappe.ui.form.on('Evento Sala', {
 
 	},
 	validate: function(frm){
-		if ( frm.doc.__islocal ) {  //usamos __islocal para validar si esta grabado o no en la DB
+		// if ( frm.doc.__islocal ) {  //usamos __islocal para validar si esta grabado o no en la DB
 				frappe.call({
 					method: "frappe.client.get_value", // AJAX call al API interno
 					args: {
@@ -17,19 +17,28 @@ frappe.ui.form.on('Evento Sala', {
 					callback: function(r) {
 						console.log(r.message)
 						let mensaje = r.message.mensaje
-						var hora_inicio = r.message.hora_inicio
-						Swal.fire({  //Usamos sweet alert
-						  type: 'success',
-						  title: 'Guardado Exitosamente',
-						  text: mensaje,
-						  footer: '<a href>Enviar Correo de Notificacion</a>'
-						})
+						comparacion(frm, r.message.hora_inicio)
+						// Swal.fire({  //Usamos sweet alert
+						//   type: 'success',
+						//   title: 'Guardado Exitosamente',
+						//   text: mensaje,
+						//   footer: '<a href>Enviar Correo de Notificacion</a>'
+						// })
 					}
 				});
-		}
+		// }
 
-		if ( frm.doc.inicio < hora_inicio ) { alert('es muy temprano')}
+
 
 
 	}
 });
+
+var comparacion = function(frm, hora_inicio){
+	let inicio_setting = parseFloat(hora_inicio)
+	let inicio_form = parseFloat(moment(frm.doc.inicio).format('hh:mm:s'))
+	if ( inicio_form < inicio_setting) {
+		frappe.msgprint('La hora de inicio de este evento no es permitida. Debes agendar despues de las ' + hora_inicio)
+	}
+
+}
